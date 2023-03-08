ad7f4fa5f98bc41f478744ba0795dd70e9348220
Marek Zelinka <mzelinka17@gmail.com>
2023-03-08 15:50:32 +0100

    setup eslint

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,14 @@
+function Welcome(props: { name: string }) {
+  const { name } = props;
+
+  return <h2>Hello, {name}!</h2>;
+}
+
 export function App() {
-  return <div />;
+  return (
+    <div>
+      <Welcome name="Marek" />
+      <Welcome name="Karen" />
+    </div>
+  );
 }
